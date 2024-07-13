0b3057f23a499fb229441b717eb66184bd3bdc10
CharlieWrMz <charliewmartinez@gmail.com>
2024-07-13 20:41:39 +0200

    MainMenuToSettinsMenu

--- JuegoMiedo/Assets/Scripts/Menu/MenuOpciones.cs
+++ JuegoMiedo/Assets/Scripts/Menu/MenuOpciones.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MenuPrincipal : MonoBehaviour
+public class MenuOpciones : MonoBehaviour
 {
     [Header("Buttons")]
     [SerializeField] private Button startButton;
