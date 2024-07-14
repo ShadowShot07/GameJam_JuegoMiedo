e1eb76584c1a17a908734cfe0fbb85f27b6c8209
enlazadordm <150435850+enlazadordm@users.noreply.github.com>
2024-07-14 06:30:31 +0200

    commit timidin
    sonidos random detrás funcionando, creo

--- JuegoMiedo/Assets/Audio/Scripts/AudioSceneController.cs
+++ JuegoMiedo/Assets/Audio/Scripts/AudioSceneController.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class AudioSceneController : MonoBehaviour
 {
     [SerializeField] private float scarySoundAverageTime;
-    [SerializeField] [Range(0f, 5f)] private float scarySoundTimeRandomize;
+    [SerializeField] [Range(0f, 10f)] private float scarySoundTimeRandomize;
     [SerializeField] private GameObject scarySoundObject;
 
     [SerializeField] private Vector3 scaryObjectRandomizePosition;
 
     private float time;
+    private float scaryTime;
 
     private Vector3 scarySoundObjectPosition;
 
@@ -20,12 +21,13 @@ public class AudioSceneController : MonoBehaviour
     {
         player = GameObject.FindGameObjectWithTag("Player");
         AudioManager.instance.StartGameAmbience();
+        scaryTime = scarySoundAverageTime + Random.Range(-scarySoundTimeRandomize, scarySoundTimeRandomize);
     }
 
     private void Update()
     {
         time += Time.deltaTime;
-        if (time >= scarySoundAverageTime)
+        if (time >= scaryTime)
         {
             PlayScarySoundFromScaryObject();
             time = 0f;
@@ -37,12 +39,23 @@ public class AudioSceneController : MonoBehaviour
         scarySoundObjectPosition = GenerateRandomPositionArroundPlayer();
         scarySoundObject.transform.position = scarySoundObjectPosition;
         AudioManager.instance.PlayScarySound(scarySoundObject);
+        scaryTime = scarySoundAverageTime + Random.Range(-scarySoundTimeRandomize, scarySoundTimeRandomize);
+        Debug.Log("RANDOM POS; " + scarySoundObjectPosition);
+        Debug.Log("RANDOM TIME; " + scaryTime);
+        Debug.Log("Forward: " + player.transform.forward);
+
     }
 
     private Vector3 GenerateRandomPositionArroundPlayer()
     {
-        Vector3 randomPosition = player.transform.position - (Vector3.Scale(player.transform.forward, scaryObjectRandomizePosition));
-        return randomPosition;
+        Vector3 randomizePosition = new Vector3(Random.Range(-1f, 1f) * scaryObjectRandomizePosition.x, 
+            Random.Range(-1f, 1f) * scaryObjectRandomizePosition.y, 
+            Random.Range(0f, 1f) * scaryObjectRandomizePosition.z);
+        
+        Vector3 objectPosition = player.transform.localPosition - player.transform.forward * randomizePosition.z + 
+            player.transform.right * randomizePosition.x + player.transform.up * randomizePosition.y;
+
+        return objectPosition;
     }
 
 
